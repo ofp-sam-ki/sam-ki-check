d8b0dc7450db3eb2e4930ea7979ba88953ee24fb
larissaeiler <154334639+larissaeiler@users.noreply.github.com>
2024-03-19 16:50:52 +0100

    Add files via upload
    checkbox Styling
    funktioniert noch nicht innerhalb der Scrollbaren Checkliste

--- src/components/check.vue
+++ src/components/check.vue
@@ -139,11 +139,12 @@ export default {
                     checkbox.id = "' + id + '-schritt-div-content";
                     checkbox.style.display = "none";
 
-                    let iconChecked = '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="secondary" class="bi bi-check2-square" viewBox="0 0 16 16"><path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z"/><path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0"/></svg>';
-                    let iconUnchecked = '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="secondary" class="bi bi-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/></svg>';
+                    let iconChecked = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="green" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/></svg>';
+                    let iconUnchecked = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/></svg>';
                     
                     let checkboxWrapper = document.createElement("label");
-                    checkboxWrapper.classList.add("checkbox-icon");
+                    checkboxWrapper.classList.add("btn", "btn-outline-secondary", "btn-lg", "menu-button", "me-md-2");
+                    checkboxWrapper.type = "button";
                     checkboxWrapper.htmlFor = checkbox.id;
 
                     checkboxWrapper.innerHTML = checkbox.checked ? iconChecked : iconUnchecked;
@@ -158,10 +159,10 @@ export default {
                         else {element.classList.remove('table-success');}
                     });
 
-                    /* view.addEventListener("click", function()
+                    view.addEventListener("click", function()
                                 {checkbox.checked = false;
                                  element.classList.remove('table-success');
-                                 checkboxWrapper.innerHTML = checkbox.checked ? iconChecked : iconUnchecked;}); */
+                                 checkboxWrapper.innerHTML = checkbox.checked ? iconChecked : iconUnchecked;});
 
                     add.appendChild(checkbox);
                     add.appendChild(checkboxWrapper);
@@ -690,7 +691,7 @@ export default {
         <div >
             <div class="mb-5 h1 text-center">Prüfauftrag</div>
 
-            <table class="table align-middle" id="injectionPointAuftragsinfos">
+            <table class="table table-responsive" style="max-height: 500px; overflow: auto; margin-left:-100px;" id="injectionPointAuftragsinfos">
             </table>
         </div>
     </div>
